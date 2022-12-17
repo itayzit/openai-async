9a1bee02fc4120073cdb7b0008789771352974af
itayzit <itay@hyro.ai>
2022-12-17 23:39:18 +0200

    add description to setup.py

--- setup.py
+++ setup.py
@@ -10,6 +10,7 @@ setuptools.setup(
     author="itayzit",
     version="0.0.1",
     author_email="itayzit@gmail.com",
+    description="A light-weight, asynchronous client for OpenAI API - text completion, image generation and embeddings.",
     long_description=_LONG_DESCRIPTION,
     long_description_content_type="text/markdown",
     url="https://github.com/itayzit/openai-async",
