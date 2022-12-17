d3d140bae000667389b49fbf3198bc9a3afacc1d
itayzit <itay@hyro.ai>
2022-12-17 22:56:29 +0200

    change setup.py to upload to pypi

--- setup.py
+++ setup.py
@@ -6,10 +6,12 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="openai-async",
-    version="0.0.0",
-    python_requires=">=3.9.5",
+    license='MIT',
+    author="itayzit",
+    author_email="itayzit@gmail.com",
     long_description=_LONG_DESCRIPTION,
     long_description_content_type="text/markdown",
+    url="https://github.com/itayzit/openai-async",
     packages=setuptools.find_namespace_packages(),
     install_requires=["httpx", "pytest"],
 )
